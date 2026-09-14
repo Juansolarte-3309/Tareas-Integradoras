a1671244f7f3491032d2f8be0a43b9184c1106b6
Juan Pablo Solarte <juanpablosolartealvarez189@gmail.com>
2026-09-15 04:39:48 +1000

    última revisión

--- Rendimiento.java
+++ Rendimiento.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 public class Rendimiento{
     public static Scanner sc = new Scanner(System.in);
 
-    public static final double notaMinima = 3.0; 
+    public static final double NOTA_MINIMA = 3.0; 
     public static int numeroEstudiantes = 0;
 
     public static void main(String args[]){
         System.out.println("ingresa el numero de estudiantes: ");
         numeroEstudiantes = sc.nextint();
 
+        while()
+        System.out.println("ingresa la nota final de cada uno de tus estudiantes: ");
+        for(i = 0; i < numeroEstudiantes.lenght; i++){
+            notasFinales[i] = numeroEstudiantes[i];
+        }
+
     }
+
+    int[] notasFinales = new int[numeroEstudiantes];
 
 }
