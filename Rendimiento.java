8d3204d8ca519bc4c4729ca67f36ce9bbf9d7517
Juan Pablo Solarte <juanpablosolartealvarez189@gmail.com>
2026-09-15 02:28:39 +1000

    solicitud de numero de estudiantes al usuario

--- Rendimiento.java
+++ Rendimiento.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 public class Rendimiento{
-    
     public static Scanner sc = new Scanner(System.in);
+
+    public static final double notaMinima = 3.0; 
+    public static int numeroEstudiantes = 0;
+
+    public static void main(String args[]){
+        System.out.println("ingresa el numero de estudiantes: ");
+        numeroEstudiantes = sc.nextint();
+
+    }
 
 }
