702ef9c41993ba7970c1964b891c2d7926c12771
Juan Pablo Solarte <juanpablosolartealvarez189@gmail.com>
2026-09-15 11:22:10 -0500

    correcion errores de sintaxtis y logica en el metodo main

--- Rendimiento.java
+++ Rendimiento.java
@@ -4,20 +4,20 @@ public class Rendimiento{
 
     public static final double NOTA_MINIMA = 3.0; 
     public static int numeroEstudiantes = 0;
+    public static double[] notasFinales;
+    
 
     public static void main(String args[]){
         System.out.println("ingresa el numero de estudiantes: ");
-        numeroEstudiantes = sc.nextint();
-
-        while()
+        numeroEstudiantes = sc.nextInt();
+        notasFinales = new double[numeroEstudiantes];
         
-        for(i = 0; i < numeroEstudiantes; i++){
-            System.out.println("ingresa la nota final de cada uno de tus estudiantes: ");
-            int notasFinales[i] = numeroEstudiantes;
+        for(int i = 0; i < numeroEstudiantes; i++){
+
+                System.out.println("ingresa la nota final de cada uno de tus estudiantes: ");
+                notasFinales[i] = sc.nextDouble();
         }
-
+        
     }
-
-    int[] notasFinales = new int[numeroEstudiantes];
 
 }
